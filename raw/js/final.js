56bb2919867ed4d6958a81938551daa5e0d0cc63
Charlie Grinsted <charlesgrinsted@gmail.com>
2017-10-18 15:03:16 +0100

    Remove unused framework bits

--- raw/js/final.js
+++ raw/js/final.js
@@ -1,15 +1,5 @@
 ( function() {
 	'use strict';
 
-	( function() {
-		// Cookie notice
-		if ( document.cookie.indexOf( 'seenCookieNotice' ) === -1 ) {
-			document.cookie = 'seenCookieNotice=yes;path=/;max-age=31536000';
-			document.body.classList.add( 'show-cookie-notice' );
-			document.querySelector( '.cookie-notice button' ).addEventListener( 'click', function() {
-				document.body.classList.remove( 'show-cookie-notice' );
-			} );
-		}
-	} )();
 
 } )();
